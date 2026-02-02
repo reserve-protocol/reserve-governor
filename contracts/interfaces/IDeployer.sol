073e54050ba4c9835b91f4ad322bcb9f64eec696
Taylor Brent <taylor.w.brent@gmail.com>
2026-02-02 17:44:16 -0400

    use interfaces only

--- contracts/interfaces/IDeployer.sol
+++ contracts/interfaces/IDeployer.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { OptimisticSelectorRegistry } from "../OptimisticSelectorRegistry.sol";
+import { IOptimisticSelectorRegistry } from "./IOptimisticSelectorRegistry.sol";
 import { IReserveOptimisticGovernor } from "./IReserveOptimisticGovernor.sol";
 import { IVetoToken } from "./IVetoToken.sol";
 
@@ -18,7 +18,7 @@ interface IReserveOptimisticGovernorDeployer {
         IReserveOptimisticGovernor.OptimisticGovernanceParams optimisticParams;
         IReserveOptimisticGovernor.StandardGovernanceParams standardParams;
         IVetoToken token;
-        OptimisticSelectorRegistry.SelectorData[] selectorData;
+        IOptimisticSelectorRegistry.SelectorData[] selectorData;
         address[] optimisticProposers;
         address[] guardians;
         uint256 timelockDelay;
