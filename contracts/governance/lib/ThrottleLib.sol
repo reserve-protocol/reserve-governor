682facda7b5323d97fa2111a58a92d19ec05dfb2
Taylor Brent <taylor.w.brent@gmail.com>
2026-02-18 14:49:33 -0300

    add proposal throttle getter

--- contracts/governance/lib/ThrottleLib.sol
+++ contracts/governance/lib/ThrottleLib.sol
@@ -17,19 +17,41 @@ library ThrottleLib {
     }
 
     function consumeProposalCharge(ProposalThrottleStorage storage proposalThrottle, address account) external {
+        (uint256 proposalsAvailable, uint256 charge) = _getProposalsAvailable(proposalThrottle, account);
+        require(proposalsAvailable >= 1, IReserveOptimisticGovernor.ProposalThrottleExceeded());
+
+        ProposalThrottle storage throttle = proposalThrottle.throttles[account];
+
+        throttle.currentCharge = charge - ((1e18 + proposalThrottle.capacity - 1) / proposalThrottle.capacity);
+        throttle.lastUpdated = block.timestamp;
+    }
+
+    function getProposalsAvailable(ProposalThrottleStorage storage proposalThrottle, address account)
+        external
+        view
+        returns (uint256 proposalsAvailable)
+    {
+        (proposalsAvailable,) = _getProposalsAvailable(proposalThrottle, account);
+    }
+
+    // === Private ===
+
+    /// @return proposalsAvailable The number of proposals available for the account
+    /// @return charge D18{1} The charge for the account
+    function _getProposalsAvailable(ProposalThrottleStorage storage proposalThrottle, address account)
+        private
+        view
+        returns (uint256 proposalsAvailable, uint256 charge)
+    {
         ProposalThrottle storage throttle = proposalThrottle.throttles[account];
 
         uint256 elapsed = block.timestamp - throttle.lastUpdated;
-        uint256 charge = throttle.currentCharge + (elapsed * 1e18) / PROPOSAL_THROTTLE_PERIOD;
+        charge = throttle.currentCharge + (elapsed * 1e18) / PROPOSAL_THROTTLE_PERIOD;
 
         if (charge > 1e18) {
             charge = 1e18;
         }
 
-        uint256 proposalsAvailable = (proposalThrottle.capacity * charge) / 1e18;
-        require(proposalsAvailable >= 1, IReserveOptimisticGovernor.ProposalThrottleExceeded());
-
-        throttle.currentCharge = charge - ((1e18 + proposalThrottle.capacity - 1) / proposalThrottle.capacity);
-        throttle.lastUpdated = block.timestamp;
+        proposalsAvailable = (proposalThrottle.capacity * charge) / 1e18;
     }
 }
